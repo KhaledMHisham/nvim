b2b3d6f150f050bcca4a32ee6c09e664c03efc57
KhaledMHisham <khaled.m.hisham@gmail.com>
2025-03-14 05:17:23 +0200

    Add rust-analyzer config and initial key-maps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -9,7 +9,8 @@ return {
         "hrsh7th/cmp-cmdline",
         "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip"
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets"
     },
 
     config = function()
@@ -33,7 +34,67 @@ return {
                             capabilities = capabilities
                         }
                     end,
-                   ["lua_ls"] = function()
+                    ["rust_analyzer"] = function()
+                        local lspconfig = require("lspconfig")
+                        lspconfig.rust_analyzer.setup {
+                            capabilities = capabilities,
+                            on_attach = function(client, bufnr)
+                                vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+
+                                local keymap_opts = { buffer = bufnr }
+
+                                -- Code navigation and shortcuts
+                                vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
+                                vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+                                vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+                                vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
+                                vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
+                                vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+                                vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
+                                vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
+                                vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+
+                                -- Set updatetime for CursorHold
+                                -- 300ms of no cursor movement to trigger CursorHold
+                                vim.opt.updatetime = 100
+
+                                -- Show diagnostic popup on cursor hover
+                                local diag_float_grp = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
+                                vim.api.nvim_create_autocmd("CursorHold", {
+                                    callback = function()
+                                        vim.diagnostic.open_float(nil, { focusable = false })
+                                    end,
+                                    group = diag_float_grp,
+                                })
+
+                                -- Goto previous/next diagnostic warning/error
+                                vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, keymap_opts)
+                                vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
+                            end,
+                            settings = {
+                                ["rust-analyzer"] = {
+                                    imports = {
+                                        granularity = {
+                                            group = "module",
+                                        },
+                                        prefix = "self",
+                                    },
+                                    cargo = {
+                                        buildScripts = {
+                                            enable = true,
+                                        },
+                                    },
+                                    procMacro = {
+                                        enable = true
+                                    },
+                                    checkOnSave = {
+                                        command = "clippy",
+                                    },
+                                }
+                            }
+                        }
+                    end,
+                    ["lua_ls"] = function()
                         local lspconfig = require("lspconfig")
                         lspconfig.lua_ls.setup {
                             capabilities = capabilities,
