65fd32b0e97e2aa1c1f5338a6fa8ce94bfe4e997
KhaledMHisham <khaled.m.hisham@gmail.com>
2025-03-14 14:39:56 +0200

    Add fidget for nvim notifications and LSP progress and add formatters for lua and rust

--- lua/vim_options.lua
+++ lua/vim_options.lua
@@ -1,10 +1,8 @@
 -- Basic settings
-vim.cmd("set expandtab")      
-vim.cmd("set tabstop=4")      
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
 -- Set leader key before loading any plugins
 vim.g.mapleader = " "
-
-
