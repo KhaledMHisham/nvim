af769f8f1ab612aaad5ed6b8233565bf39841158
KhaledMHisham <khaled.m.hisham@gmail.com>
2025-03-14 05:57:37 +0200

    Update the keymaps for lsp buffer and diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -44,15 +44,15 @@ return {
                                 local keymap_opts = { buffer = bufnr }
 
                                 -- Code navigation and shortcuts
-                                vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
-                                vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
-                                vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
-                                vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
-                                vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
-                                vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
-                                vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
-                                vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
                                 vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+                                vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+                                vim.keymap.set("n", "td", vim.lsp.buf.type_definition, keymap_opts)
+                                vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+                                vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, keymap_opts)
+                                vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, keymap_opts)
+                                vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, keymap_opts)
+                                vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, keymap_opts)
+                                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, keymap_opts)
 
                                 -- Set updatetime for CursorHold
                                 -- 300ms of no cursor movement to trigger CursorHold
